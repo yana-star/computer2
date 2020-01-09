d6d10f80eccf3d42a965536af3e4281fe082074e
yana-star <59680946+yana-star@users.noreply.github.com>
2020-01-09 13:54:02 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,1 +1,31 @@
-hhhh
+<?
+session_start();
+	if(!isset($_SESSION['member'])){
+		header("location:login.php");
+	}
+	$load_page = "home";
+	include 'include/header.php';
+?>
+
+
+
+<?
+		include 'include/top-menu.php';
+?>
+<!----------------------------------------------------------------->
+  <? if($_SESSION['user_role']=="admin"){ ?>
+<div class="container-fluid" style="background-color:#FFF;">
+
+
+<div class="row">
+	
+	<div class="col-lg-12">
+<?
+include 'start-page.php';
+echo '</div></div></div>';
+}
+else {
+	header("location:repaire.php");
+}
+	include 'include/footer.php';
+?>
